6cd1df2ccca53e8934ae07ae6b7e4ea973b3ee35
Muhammad Khalil <pkhalisoft@gmail.com>
2023-10-27 21:14:09 +0100

    more config, fixed TS and added error codes

---
+++ src/utils/logger/logger.ts
@@ -0,0 +1,27 @@
+import winston from "winston";
+import { options } from "@configs/loggerConfig.json";
+export class Logger {
+	private logger: winston.Logger;
+
+	private static instance: Logger;
+	private constructor() {
+		this.logger = winston.createLogger({
+			transports: [
+				new winston.transports.Console(options.console),
+				new winston.transports.File(options.file),
+			],
+		});
+	}
+
+	public static getLoggerInstance() {
+		if (!Logger.instance) {
+			Logger.instance = new Logger();
+		}
+		return Logger.instance;
+	}
+
+	public static getLogger() {
+		let _logger = Logger.getLoggerInstance();
+		return _logger.logger;
+	}
+}
